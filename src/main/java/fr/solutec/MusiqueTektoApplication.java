44fb6bced061c2d01fe185385800e95f3a0a719a
ADM RPZ <adrien.moranne@consultants-solutec.fr>
2023-01-12 15:29:41 +0100

    Création Table User

--- src/main/java/fr/solutec/MusiqueTektoApplication.java
+++ src/main/java/fr/solutec/MusiqueTektoApplication.java
@@ -1,15 +1,24 @@
 package fr.solutec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.entities.User;
+import fr.solutec.repository.UserRepository;
+
 @SpringBootApplication
 public class MusiqueTektoApplication {
-
+	@Autowired
+	private UserRepository userRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(MusiqueTektoApplication.class, args);
-		
 		System.out.println("lancement terminé");
 	}
-
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		User u1 = new User(null, "Test", "Test", "Test", "Test", "Test", "Test");
+		userRepo.save(u1);
+		
+	}
 }
--- src/main/java/fr/solutec/entities/User.java
+++ src/main/java/fr/solutec/entities/User.java
@@ -1,5 +1,24 @@
 package fr.solutec.entities;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor @Data 	//Crée les constructeurs sans et avec arguments, puis les getter et setter.
+@Entity
+public class User {
+	
+	@Id @GeneratedValue							//Le id met la ligne suivante en primary key, le GeneratedValue met en auto Increment
+	private Long id;
+	
+	private String login;
+	private String password;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String mail;
 }

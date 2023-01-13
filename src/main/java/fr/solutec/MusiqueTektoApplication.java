841a45b626f6f0ac382343c94d02c9665c85a49e
dsi <dsi@A0002885>
2023-01-13 11:04:56 +0100

    Complément des datas pour la database

--- src/main/java/fr/solutec/MusiqueTektoApplication.java
+++ src/main/java/fr/solutec/MusiqueTektoApplication.java
@@ -8,9 +8,15 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import fr.solutec.entities.Artiste;
 import fr.solutec.entities.Commandes;
+import fr.solutec.entities.Commentaires;
+import fr.solutec.entities.Evenement;
+import fr.solutec.entities.Lieu;
 import fr.solutec.entities.User;
 import fr.solutec.repository.ArtisteRepository;
 import fr.solutec.repository.CommandesRepository;
+import fr.solutec.repository.CommentairesRepository;
+import fr.solutec.repository.EvenementRepository;
+import fr.solutec.repository.LieuRepository;
 import fr.solutec.repository.UserRepository;
 
 @SpringBootApplication
@@ -21,6 +27,12 @@ public class MusiqueTektoApplication implements CommandLineRunner {
 	private ArtisteRepository artisteRepo;
 	@Autowired
 	private CommandesRepository commandesRepo;
+	@Autowired
+	private CommentairesRepository commentairesRepo;
+	@Autowired
+	private EvenementRepository evenementRepo;
+	@Autowired
+	private LieuRepository lieuRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(MusiqueTektoApplication.class, args);
 		System.out.println("lancement terminé");
@@ -30,11 +42,30 @@ public class MusiqueTektoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "Test", "Test", "Test", "Test", "Test");
-		User u2 = new User(null, "Test", "Test", "Test", "Test", "Test");
+		User u2 = new User(null, "LOL", "Test", "Test", "Test", "Test");
 		userRepo.save(u1);
 		userRepo.save(u2);
 		Commandes c1 = new Commandes(null,"Test",u1);
+		Commandes c2 = new Commandes(null,"Test",u2);
 		commandesRepo.save(c1);
-
+		commandesRepo.save(c2);
+		Artiste a1 = new Artiste(null,"A1","A2",3,true,"A3","A4");
+		Artiste a2 = new Artiste(null,"A5","A6",3,true,"A7","A8");
+		artisteRepo.save(a1);
+		artisteRepo.save(a2);
+		Lieu l1 = new Lieu(null,"L1","L2",314);
+		Lieu l2 = new Lieu(null,"L1","L2",314);
+		lieuRepo.save(l1);
+		lieuRepo.save(l2);
+		Evenement e1 = new Evenement(null,"E1",(float) 314,l1,a1);
+		Evenement e2 = new Evenement(null,"E2",(float) 315,l2,a2);
+		evenementRepo.save(e1);
+		evenementRepo.save(e2);
+		Commentaires co1 = new Commentaires(null,"C1",e1,u1);
+		Commentaires co2 = new Commentaires(null,"C2",e2,u2);
+		commentairesRepo.save(co1);
+		commentairesRepo.save(co2);
+		
+		
 	}
 }

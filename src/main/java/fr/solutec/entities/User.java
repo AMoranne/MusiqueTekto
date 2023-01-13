5b4d5b1f9d0fa56801b4728aa6bfdb67a1910fba
33624 <33624@LAPTOP-LEMJ67OE>
2023-01-13 12:28:12 +0100

    artiste rest api

--- src/main/java/fr/solutec/entities/User.java
+++ src/main/java/fr/solutec/entities/User.java
@@ -20,4 +20,6 @@ public class User {
 	private String nom;
 	private String prenom;
 	private String mail;
+	
+	
 }
--- src/main/java/fr/solutec/rest/ArtisteRest.java
+++ src/main/java/fr/solutec/rest/ArtisteRest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Artiste;
@@ -22,8 +25,19 @@ public class ArtisteRest {
 	public Optional<Artiste> getById(@PathVariable Long id) {
 		return artisteRepo.findById(id);
 	}
-	
+	@GetMapping("artiste/nom/{nom}")
 	public List<Artiste> getByNom(@PathVariable String nom){
 		return artisteRepo.findByNom(nom);
 	}
+	@PostMapping("artiste/{id}")
+	public Artiste createArtiste(@RequestBody Artiste a) {
+		return artisteRepo.save(a);	
+	}
+	
+	@PutMapping("artiste/{id}")
+	public Artiste modifArtiste(@RequestBody Artiste a, @PathVariable Long id) {
+		a.setId(id);
+		return artisteRepo.save(a);
+	}
+	
 }

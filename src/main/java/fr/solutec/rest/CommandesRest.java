59c48b7c33fa3fae245ca33c3ffb2d0e216fdcea
Syo-Nara <souheil.naouel@gmail.com>
2023-01-13 14:00:57 +0100

    CommandesRest basic

---
+++ src/main/java/fr/solutec/rest/CommandesRest.java
@@ -0,0 +1,14 @@
+package fr.solutec.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.repository.CommandesRepository;
+
+@RestController 
+public class CommandesRest {
+	
+	private CommandesRepository commandesRepo;
+	
+	
+
+}

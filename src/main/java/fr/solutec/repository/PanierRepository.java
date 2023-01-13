804b2da210a61afbc0d081e115a379a5b8a6b5a6
Teckto78 <doryan.smague@gmail.com>
2023-01-13 12:54:10 +0100

    Ajout du PanierRest avec les API de base

--- src/main/java/fr/solutec/repository/PanierRepository.java
+++ src/main/java/fr/solutec/repository/PanierRepository.java
@@ -1,10 +1,13 @@
 
 package fr.solutec.repository;
 
+
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Panier;
 
 public interface PanierRepository extends CrudRepository <Panier, Long> {
+	
 
 }

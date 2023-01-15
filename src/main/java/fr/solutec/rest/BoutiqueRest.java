4fa9523fbe799679b52359f4da4c2eb4196835e9
hanssen-belen <belen.hanssen.marin@gmail.com>
2023-01-15 21:25:12 +0100

    Merge branch 'master' of https://github.com/AMoranne/MusiqueTekto

--- src/main/java/fr/solutec/rest/BoutiqueRest.java
+++ src/main/java/fr/solutec/rest/BoutiqueRest.java
@@ -2,11 +2,13 @@ package fr.solutec.rest;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import fr.solutec.entities.Produits;
 
@@ -19,14 +21,16 @@ public class BoutiqueRest {
 	@Autowired
 	private BoutiqueRepository boutiqueRepo;
 	
-	@GetMapping("produits/{designation}")
+	@GetMapping("produits/{designation}")// retrouver un produit par son nom
 	public List<Produits> getByDesignation(@PathVariable String designation){
 		return boutiqueRepo.findByDesignation(designation);
 		}
 	
-	@GetMapping("produits/all")
+	@GetMapping("produits/all")// retrouver tous les produits
 	public Iterable<Produits> getAllProduits(){
 		return boutiqueRepo.findAll();
 	}	
+	
+
 	
 }

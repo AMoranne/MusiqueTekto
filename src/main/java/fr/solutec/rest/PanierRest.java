d40a8e19967db1e4002eddfd630b83adbbab2130
AMoranne <adrien.moranne@gmail.com>
2023-01-17 16:22:16 +0100

    MON BACK EST CASSÉ

--- src/main/java/fr/solutec/rest/PanierRest.java
+++ src/main/java/fr/solutec/rest/PanierRest.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Panier;
-import fr.solutec.entities.Produits;
 import fr.solutec.repository.PanierRepository;
 
 
--- src/main/java/fr/solutec/rest/PlaylistRest.java
+++ src/main/java/fr/solutec/rest/PlaylistRest.java
@@ -3,7 +3,6 @@ package fr.solutec.rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,10 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.solutec.entities.Morceau;
 import fr.solutec.entities.Playlist;
-import fr.solutec.entities.User;
-import fr.solutec.repository.MorceauRepository;
 import fr.solutec.repository.PlaylistRepository;
 
 @RestController @CrossOrigin("*")

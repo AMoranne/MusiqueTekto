14dfc3cd8d24472a27f29e9bf5c9554b506b210b
mzaabay <mwantoymou.zaabay@gmaill.com>
2023-01-18 20:14:25 +0100

    r

--- src/main/java/fr/solutec/rest/PanierRest.java
+++ src/main/java/fr/solutec/rest/PanierRest.java
@@ -12,20 +12,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/AMoranne/MusiqueTekto
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Panier;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/AMoranne/MusiqueTekto
 import fr.solutec.repository.PanierRepository;
 
 
